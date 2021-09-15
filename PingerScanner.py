0d0f5081838466d5d90033464edac28b4209ce2b
Esad-U <aesadugur@gmail.com>
2021-09-15 23:43:58 +0300

    Tarama koduna sola dönme durumu eklendi

--- PingerScanner.py
+++ PingerScanner.py
@@ -15,10 +15,16 @@ class PingerScanner():
     def turn_right(self):
         pass
 
+    def turn_left(self):
+        pass
+
     def main(self, heading, front_conf, front_dist, right_conf, right_dist):
         # ana tarama kodu
         if front_conf > 90 and front_dist > 500:
             self.heading = self.set_heading(heading)
             self.forward(self.heading)
         else:
-            self.turn_right()
+            if right_conf > 90 and right_dist > 500:
+                self.turn_left()
+            elif right_conf > 90 and right_dist < 500:
+                self.turn_left()
